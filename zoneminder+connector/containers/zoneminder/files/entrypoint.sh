eed5e266077fa6a6529f1ffa6e79a66524315225
François Déchelle <fdechelle@teclib.com>
2026-01-14 15:56:00 +0100

    Add shell script for JSON logging of events

--- zoneminder+connector/containers/zoneminder/files/entrypoint.sh
+++ zoneminder+connector/containers/zoneminder/files/entrypoint.sh
@@ -5,11 +5,17 @@ set -e
 # install IDMEFv2 connectors
 cd /idmefv2-connectors
 pip install --force-reinstall --editable .
+# simplify path of script
+rm -f /zm2json.sh
+ln -s /idmefv2-connectors/idmefv2/connectors/zoneminder/zm2json.sh /zm2json.sh
 
 # Prepare log file (path is defined in connector configuration file)
-mkdir -p /var/log/idmefv2
-touch /var/log/idmefv2/zoneminder-connector.log
-chown www-data.www-data -R /var/log/idmefv2
+mkdir -p /var/log/zmjson/
+touch /var/log/zmjson/events.json
+chown www-data.www-data -R /var/log/zmjson/
+
+# start the connector
+python3 -m idmefv2.connectors.zoneminder -c /etc/zoneminder-idmefv2.conf &
 
 # run the zobeminder startup script
 /etc/my_init.d/startup.sh
--- zoneminder+connector/containers/zoneminder/files/zm2idmefv2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-LOGFILE=/var/log/idmefv2/zoneminder-connector.log
-echo "ARGS $*" >> $LOGFILE
-/usr/bin/python3 -m idmefv2.connectors.zoneminder -c /etc/zoneminder-idmefv2.conf "$1" "$2" "$3" "$4" "$5" >> $LOGFILE 2>&1
-echo "RET $?" >> $LOGFILE
-
-exit 0
